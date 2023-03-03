e3c7dd51e05bb7ade7433f127205853ccc351099
MuhammedIRishad <rishad551@gmail.com>
2023-03-03 19:13:00 +0530

    Basic dart program-1

---
+++ sample1.dart
@@ -0,0 +1,22 @@
+//1. Accept a char input from the user and display it on the console.
+
+// import 'dart:io';
+
+// void main() {
+//   print('Enter your name');
+//   var name = stdin.readLineSync();
+
+//   print('Hello Mr. $name. Welcome to Level-X');
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   print('Enter your name');
+//   var name = stdin.readLineSync();
+
+//   print('hello mr $name. Welcome to Dart');
+// }
+
+// import 'dart:io';
+
